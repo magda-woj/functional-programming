20bfe3ca38547196a55ca9a0fe77d7899e4ff9b5
magda-woj <m.wojtowicz@hotmail.com>
2023-11-12 22:05:51 +0100

    dodane polecenia do lab5

--- lab5.hs
+++ lab5.hs
@@ -1,6 +1,9 @@
 import Data.Char (isLower)
 import Data.Binary.Get (label)
--- zad1
+-- zad1 
+-- Napisać używając map i filter funkcję
+-- maleLitery :: [String] -> [String]
+-- przyjmującą listę napisów i zwracającą listę złożoną tylko i wyłącznie z małych liter alfabetu oryginalnych napisów.
 maleLiteryString :: String -> String
 maleLiteryString s = [x | x <- ['a'..'z'], elem x s]
 
@@ -8,6 +11,9 @@ maleLitery :: [String] -> [String]
 maleLitery l = map maleLiteryString l
 
 -- zad2
+-- Napisać używając map i filter funkcję
+-- dlugoscPalindromow :: [String] -> Int
+-- przyjmującą listę napisów i zwracającą sumę długości tych z nich, które są palindromami.
 jestPalindromem :: String -> Bool
 jestPalindromem s = reverse s == s
 
@@ -15,6 +21,10 @@ dlugoscPalindromow :: [String] -> Int
 dlugoscPalindromow l = sum (map length (filter jestPalindromem l))
 
 -- zad3
+-- Napisać używając iterate funkcję
+-- fib :: (Integer,Integer) -> [(Integer,Integer)]
+-- przyjmującą parę liczb i zwracającą nieskończoną listę, której kolejny element (x′, y′) powstaje z poprzedniego (x, y) w takisposób, że x jest równy y, 
+-- a y′jest sumą x i y. Używając tejfunkcji wygenerować nieskończoną listę zawierającą kolejneelementy ciągu Fibonacciego (od 0,1,1,...).
 fiboPair :: (Integer, Integer) -> (Integer, Integer)
 fiboPair (x, y) = (y, x+y)
 
@@ -28,25 +38,38 @@ fibonacci :: [Integer]
 fibonacci = firsts (fib (0, 1))
 
 -- zad4
+-- Zdefiniować funkcję
+-- dlugosc :: [a] -> Int
+-- działającą jak length, czyli zwracającą długość listy, używając funkcji map i sum (i nie używając length).
 dlugosc :: [a] -> Int
 dlugosc l = sum (map (\c -> 1) l)
 
 -- -- zad5
-addLetter :: Char -> String -> String
-addLetter x s = [x] ++ s
+-- Napisać funkcję
+-- slowaDlugosci :: Char -> Char -> Integer -> [String]
+-- przyjmującą dwa znaki oraz liczbę i zwracającą wszystkie słowa podanej długości, których literami są podane znaki. Przykładowo
+-- dla ’a’ ’b’ 2 wynikiem powinno być [”aa”,”ab”,”ba”,”bb”].Słowa nie muszą być podane w tej kolejności, ważne, żeby byływszystkie
 
 slowaDlugosci :: Char -> Char -> Int -> [String]
 slowaDlugosci x y 1 = [[x],[y]]
 slowaDlugosci x y a = (map (x:) (slowaDlugosci x y (a-1))) ++ (map (y:) (slowaDlugosci x y (a-1)))
 
--- zad7
+-- -- zad7
+-- Używając zipWith zaimplementować map i zip.
+-- Wskazówka: Jeśli listy nie są równej długości, elementy dłuższej
+-- ”bez pary” z elementem listy krótszej są pomijane.
 myZip :: [a] -> [b] -> [(a, b)]
 myZip = zipWith (\x y -> (x,y))
 
 myMap :: (a -> b) -> [a] -> [b]
 myMap fun l = zipWith (\ x y -> fun(x)) l [0..] 
 
--- zad8
+-- -- zad8
+-- Zaimplementować funkcję o sygnaturze
+-- eratosthenes :: Integer -> [Integer]
+-- przyjmującą liczbę n i zwracającą listę liczb pierwszych nie
+-- większych niż n obliczoną metodą Sita Eratostenesa.
+
 sieve :: Integer -> [Integer] -> [Integer]
 sieve p l = if p*p > last l then l
 else
