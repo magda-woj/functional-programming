fccb871217714c0aa5e82c70fe89083139e0fa71
magda-woj <m.wojtowicz@hotmail.com>
2023-11-12 22:09:34 +0100

    dodane polecenia do lab2

--- lab2.hs
+++ lab2.hs
@@ -1,9 +1,15 @@
 import Language.Haskell.TH (safe)
 import Control.Monad (liftM4)
 -- import System.Win32 (xBUTTON1)
--- zad1
+-- -- zad1
+-- Napisać funkcję o sygnaturze
+-- jestPunktemStalym :: (Eq a) => a -> (a -> a) -> Bool
+-- sprawdzającą, czy punkt podany jako pierwszy argument jest
+-- punktem stałym funkcji podanej jako drugi argument
+
 jestPunktemStalym :: Eq a => a -> (a -> a) -> Bool
 jestPunktemStalym x f = x == f x
+
 -- for testing
 -- funkcja :: Int -> Int
 -- funkcja x = x*x
@@ -12,42 +18,37 @@ jestPunktemStalym x f = x == f x
 -- jestPunktemStalymfunkcja x = jestPunktemStalym x funkcja
 
 -- zad2
+-- Napisać funkcję dwuargumentową (możliwie najogólniejszą), która
+-- przyjmuje liczby x, y oraz zwraca napis, który informuje nas, czy
+-- iloczyn x i y jest większy od ich sumy. W napisie mają pojawić się
+-- liczby x i y
+
 sprawdzIloczyn :: (Num a, Show a, Ord a) => a -> a -> String
 sprawdzIloczyn a b = "Iloczyn " ++ show a ++ " " ++ show b ++ " jest " ++ (if a*b > a+b then "większy" else "mniejszy") ++ " niż suma"
 
 -- zad3
+-- Zdefiniować nieskończoną listę dodatnich rozwiązań układu
+-- kongruencji:
+-- x ≡ 1 ( mod 6)
+-- x ≡ 4 ( mod 7)
+-- x ≡ 3 ( mod 8)
+
 zad3 :: [Int]
 zad3 = [x | x <- [1..], x `mod` 6 == 1, x `mod` 7 == 4, x `mod` 8 == 3]
 
 -- zad4
+-- Zdefiniować funkcję dzielniki :: Int -> [Int] zwracającą
+-- listę dzielników liczby z wykorzystaniem funkcji z dzisiejszych
+-- materiałów (por. zadanie do pierwszych ćwiczeń z liczbą
+-- doskonałą)
 dzielniki :: Int -> [Int]
 dzielniki n = [x | x <- [1..n], n `mod` x == 0]
 
--- zad5
-
-f1 :: (Int, Int) -> Bool
-f1 (x,y) = x >= 0 && y >= 0
-
-f2 :: (Int, Int) -> Bool
-f2 (x,y) = x <= 0 && y >= 0
-
-f3 :: (Int, Int) -> Bool
-f3 (x,y) = x <= 0 && y <= 0
-
-f4 :: (Int, Int) -> Bool
-f4 (x,y) = x >= 0 && y <= 0
-
-
-ktoraCwiartka :: [(Int,Int)] -> Int
-ktoraCwiartka list = 
-    let (l1, l2, l3, l4) = (filter f1 list, filter f2 list, filter f3 list, filter f4 list) in
-    max (max (length l1) (length l2)) (max (length l3) (length l4))
-    
-
--- zad6
-
--- podlisty :: [Integer] -> [[Integer]]
-
 -- zad7
+-- Napisać funkcję
+-- przeksztalcListe :: (Int -> Int) -> [Int] -> [Int]
+-- przyjmującą funkcję oraz listę liczb naturalnych, która wykonuje
+-- podaną funkcję na każdym elemencie listy (czyli dla funkcji f oraz
+-- listy [x1, . . . , xn] mamy otrzymać [f (x1), . . . , f (xn)]).
 przeksztalcListe :: (Int -> Int) -> [Int] -> [Int]
 przeksztalcListe f xs = [f x | x <- xs]
