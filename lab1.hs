ea4060fd925035b8fa8de85f478c9044ec1eb515
magda-woj <m.wojtowicz@hotmail.com>
2023-11-12 21:57:07 +0100

    dodane polecenia do lab1

--- lab1.hs
+++ lab1.hs
@@ -1,11 +1,15 @@
--- zad1
+-- zad1 Napisać funkcję dwuargumentową, która przyjmuje liczby x, y oraz zwraca wartość (x^2+2xy)/y^2
+-- Wykorzystując napisaną definicję funkcji oraz ustalenie wartości jednym z argumentów zdefiniować funkcję przyjmującą liczbę x oraz zwracającą (25 + 10y)/y^2 
 zad1_1 :: Double -> Double -> Double
 zad1_1 x y = if y==0 then error "dzielenie przez 0!" else ((x*x) + (2*x*y))/(y*y)
 
 zad1_2:: Double -> Double
 zad1_2 y = zad1_1 5 y
 
--- zad2
+-- zad2 Wykorzystując funkcję 
+-- sumaWartosci :: (Int -> Int) -> (Int -> Int) -> Int -> Int -> Int 
+-- sumaWartosci f g x y = (f x) + (g y)
+-- zdefiniować funkcję, która przyjmuje liczby x, y oraz zwraca wartość 2x + 3y
 sumaWartosci :: (Int -> Int) -> (Int -> Int) -> Int -> Int -> Int
 sumaWartosci f g x y = f x + g y
 
@@ -18,13 +22,18 @@ razyTrzy x = 3*x
 zad2 :: Int -> Int -> Int
 zad2 = sumaWartosci razyDwa razyTrzy
 
--- zad3
+-- zad3 Zapisać funkcję
+-- ocena :: Double -> String
+-- ocena 2.0 = "niezaliczone"
+-- ocena 5.0 = "brawo!"
+-- ocena x = "wpisane masz " ++ show x
+-- stosując dozory (guards) zamiast dopasowania do wzorca.
 ocena :: Double -> String
 ocena x | x==2.0 = "niezaliczone"
         | x==5.0 = "brawo!"
         | otherwise = "wpisane masz " ++ show x
 
--- zad4
+-- zad4 Napisać funkcję obliczającą s(n, k) - liczbę Stirlinga I rodzaju.
 
 s:: Int -> Int -> Int
 s n k   | n==0 && k == 0 = 1
@@ -32,10 +41,19 @@ s n k   | n==0 && k == 0 = 1
         | n==k = 1
         | otherwise = s (n-1) k*(n-1) + s (n-1) (k-1)
 
+-- Napisać stosując rekurencję funkcję o sygnaturze
+-- iloczynListy :: [Integer] -> Integer
+-- która przyjmuje listę liczba naturalnych i zwraca jej iloczyn.
 iloczynListy :: [Integer] -> Integer
 iloczynListy list = if list == [] then 1 else head list*iloczynListy (tail list)
 
 -- zad5
+-- Napisać funkcję
+-- merge :: [Int] -> [Int] -> [Int]
+-- łączącą dwie posortowane niemalejąco listy w jedną posortowaną
+-- niemalejąco listę. Używając tej funkcji napisać funkcję
+-- mergeSort :: [Int] -> [Int]
+-- sortującą niemalejąco listę za pomocą algorytmu merge sort
 
 merge :: [Int] -> [Int] -> [Int]
 merge left right    | left == [] = right
@@ -56,7 +74,10 @@ mergeSort list =
         let (left, right) = devide list in
             merge (mergeSort left) (mergeSort  right)
 
--- zad6
+-- -- zad6
+-- Napisać funkcję
+-- czyDoskonala :: Int -> Bool
+-- sprawdzającą, czy podana liczba jest doskonała
 
 dzielniki :: (Int, [Int], Int) -> [Int]
 dzielniki(n, list, count)
